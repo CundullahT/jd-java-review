9676d46e03d8f243445be1aaca279322746226bc
CundullahT <cundullahterzioglu.it@gmail.com>
2020-11-03 04:07:07 +0100

    MapEntry

---
+++ src/main/java/com/cybertek/lambda/sorting/Employee.java
@@ -0,0 +1,16 @@
+package com.cybertek.lambda.sorting;
+
+import com.cybertek.oop.encapsulation.Role;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Employee {
+    private Integer employeeId;
+    private String employeeFirstName;
+    private String employeeLastName;
+    private String employeeCity;
+    private Role role;
+
+}
